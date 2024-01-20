c230e0c7d5faaab211c792569ccc2b44057a54a8
Gracie Gould <gracieg@scan123.com>
2024-01-19 23:18:43 -0800

    Co-authored-by: Matthew Proteau <Mattprotz@users.noreply.github.com>

---
+++ gracie.js
@@ -0,0 +1,16 @@
+
+
+function gDiv() {
+    let gDiv = document.getElementById('graciesDiv');
+}
+function mDiv() {
+    let mDiv = document.getElementById('matthewsDiv');
+}
+function cDiv() {
+    let cDiv = document.getElementById('cartersDiv');
+}
+
+function textBox() {
+    let textBox = document.getElementById('textBox');
+}
+
---
+++ matthew.js
@@ -0,0 +1,5 @@
+function Apple() {
+    const favoriteList=[
+
+    ];
+}
